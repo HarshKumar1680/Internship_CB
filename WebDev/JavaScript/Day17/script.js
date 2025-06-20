75d899aff8fe7996ab64c810142434d45fd9c2e8
HarshKumar1680 <harshkumar1680@gmail.com>
2025-06-20 17:51:28 +0530

    Create script.js

---
+++ WebDev/JavaScript/Day17/script.js
@@ -0,0 +1,23 @@
+run(); //hoisting example
+// This will work due to hoisting, as the function declaration is moved to the top
+
+function run() {
+    console.log("Hello, World!");
+}
+// run();
+//function assignment
+//Named function expression
+
+// stand(); not defined in assignment context
+let stand = function walk () {
+    console.log("Walking...");
+}
+console.log(stand); 
+stand();  // This will work as the function is assigned to a variable
+
+//annonymous function
+let greet = function() {
+    console.log("Hello, Anonymous Function!");
+}
+greet(); // This will work as the function is called after its declaration
+
