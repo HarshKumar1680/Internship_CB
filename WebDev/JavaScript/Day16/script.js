2100de178e65b979fc3764d620f9458bd2c912fc
HarshKumar1680 <harshkumar1680@gmail.com>
2025-06-20 14:57:35 +0530

    Create script.js

---
+++ WebDev/JavaScript/Day16/script.js
@@ -0,0 +1,85 @@
+// let a = Math.random();
+// console.log(a);
+
+// let b = Math.PI;
+// console.log(b);
+
+// let c = Math.round(1.8);
+// console.log(c);
+
+// Math.max(2,34,54);
+// Math.min(2,34,54);
+
+// Math.abs(-3);
+
+
+// let lastName = 'Babbar';
+
+
+// let lastName1 = new String('Babbar')
+
+// console.log(lastName.includes('Ba'));
+// console.log(lastName.startsWith('Babb'));
+// console.log(lastName.startsWith('Babb'));
+
+// let message = 'this is my first message';
+// let words = message.split(' ');
+// console.log(words);
+
+// let date = new Date();
+// console.log(date);
+
+// date.getDay;
+// date.setDate;
+
+// let numbers = [1,2,3,4,5];
+// console.log(numbers);
+
+//insertion
+//  console.log(numbers.unshift(6));
+//  console.log(numbers);
+//  console.log(numbers.splice(2,0,'a','b'));
+//  console.log(numbers);
+//  console.log(numbers.push(8));
+//  console.log(numbers);
+
+//search
+
+// console.log(numbers.indexOf(9));
+// console.log(numbers.indexOf(1,5));
+
+// console.log(numbers.includes(9));
+
+// let courses = [
+//     {no:1, name:'Harsh'  },
+//     {no:2, name:'Nisha'  }
+// ];
+// console.log(courses);
+
+// console.log(courses.indexOf({no:1, name:'Harsh'}));
+// console.log(courses.includes({no:1, name:'Harsh'})); =>bot method giving us wrong output so we use callback function method
+
+// let course = courses.find(function(course){
+//     return course.name === 'Nisha';
+// });
+// console.log(course);
+
+//arrow function/predicate function
+
+// let course = courses.find(course => course.name ==='Nisha');
+// console.log(course);
+
+let numbers = [1,2,3,4,5];
+
+
+
+
+
+
+
+
+
+
+
+
+
