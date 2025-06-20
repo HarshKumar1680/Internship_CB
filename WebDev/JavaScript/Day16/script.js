75dca698b5720a2321ff8683488b4471c34ace5d
HarshKumar1680 <harshkumar1680@gmail.com>
2025-06-20 17:51:11 +0530

    Update script.js

--- WebDev/JavaScript/Day16/script.js
+++ WebDev/JavaScript/Day16/script.js
@@ -69,7 +69,67 @@
 // let course = courses.find(course => course.name ==='Nisha');
 // console.log(course);
 
-let numbers = [1,2,3,4,5];
+//REMOVING AN ELEMENT
+// let numbers = [1,2,3,4,5];
+// console.log(numbers.pop(4));
+// console.log(numbers.shift(3));
+// console.log(numbers.splice(2,1));
+// console.log(numbers);
+// while(numbers.length > 1) {
+//   console.log(numbers.pop());
+// }
+// console.log(numbers);
+ //empty an array
+//  let numbers2 = numbers;
+//  numbers = [];
+//  console.log(numbers2); // This will still show the original array as both variables point to the same array in memory.
+//  console.log(numbers); // This will show an empty array as we have reassigned the variable to an empty array.
+
+
+//COMBINING ARRAYS
+// let first = [1, 2, 3];
+// let second = [4, 5, 6];
+// let combined = first.concat(second);
+// console.log(combined); // Output: [1, 2, 3, 4, 5, 6]
+
+// //SLICING AN ARRAY
+// let sliced = combined.slice(2, 5);
+// console.log(sliced); // Output: [3, 4, 5]
+
+// // SPREAD OPERATOR
+// let spreadCombined = [...first, ...second];
+// console.log(spreadCombined); // Output: [1, 2, 3, 4, 5, 6]
+
+// // SPREAD OPERATOR WITH OBJECTS     
+// let obj1 = { a: 1, b: 2 };
+// let obj2 = { c: 3, d: 4 };
+// let combinedObj = { ...obj1, ...obj2 };
+// console.log(combinedObj); // Output: { a: 1, b: 2, c: 3, d: 4 }
+
+//ITERATING OVER AN ARRAY
+// let numbers = [1, 2, 3, 4, 5];
+// Using forEach
+// numbers.forEach(number=> console.log(number));
+// numbers.sort();
+// console.log(numbers);
+// let numbers = [1, 2, -3, -4, 5];
+
+// let filtered = numbers.filter( value => value >= 0
+// );
+// console.log(filtered);
+
+let number = [2,3,4,5]
+
+let items = number.map(function(value){
+    return 'student_no' + value;
+})
+console.log(items);
+
+
+
+
+
+
 
 
 
