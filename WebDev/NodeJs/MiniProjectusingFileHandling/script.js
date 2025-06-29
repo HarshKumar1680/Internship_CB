cf5a5f99885cce4e64c5bdebe1d55875c99dc245
HarshKumar1680 <harshkumar1680@gmail.com>
2025-06-29 18:45:33 +0530

    restoring image from text file

--- WebDev/NodeJs/MiniProjectusingFileHandling/script.js
+++ WebDev/NodeJs/MiniProjectusingFileHandling/script.js
@@ -1,14 +1,31 @@
 const fs = require('fs');
 
-fs.readFile('Image.jpg',{
-    encoding:'base64'
-},(err,data)=>{
-    if(err) return console.log(err);
-    console.log(data);
-})
+// fs.readFile('Image.jpg',{
+//     encoding:'base64'
+// },(err,data)=>{
+//     if(err) return console.log(err);
+//     console.log(data);
+// })
 
+
+// // let file = fs.readFileSync('image.jpg');
+// // console.log(file);
+// // let str = file.toString('base64');
+// // console.log(str);
+
+// write or storing the image in a  file
 
 // let file = fs.readFileSync('image.jpg');
 // console.log(file);
 // let str = file.toString('base64');
-// console.log(str);
+
+// fs.writeFileSync('image1.txt',str);
+str = fs.readFileSync('image.txt',{encoding:'utf-8'});
+let buffer = Buffer.from(str,'base64');
+console.log(buffer);
+fs.writeFileSync('Image1.jpg',buffer);
+
+
+
+
+
