fa36e16f426766db17145bd0e6eddb5d3eba97b0
HarshKumar1680 <harshkumar1680@gmail.com>
2025-06-30 16:31:47 +0530

    Initial commit with proper structure

--- WebDev/NodeJs/MiniProjectusingFileHandling/script.js
+++ WebDev/NodeJs/MiniProjectusingFileHandling/script.js
@@ -1,44 +1,64 @@
-const fs = require('fs');
-const Jimp = require('jimp');
-// fs.readFile('Image.jpg',{
-//     encoding:'base64'
-// },(err,data)=>{
-//     if(err) return console.log(err);
-//     console.log(data);
-// })
+// const fs = require('fs');
+// const Jimp = require('jimp');
+// // fs.readFile('Image.jpg',{
+// //     encoding:'base64'
+// // },(err,data)=>{
+// //     if(err) return console.log(err);
+// //     console.log(data);
+// // })
 
+
+// // // let file = fs.readFileSync('image.jpg');
+// // // console.log(file);
+// // // let str = file.toString('base64');
+// // // console.log(str);
+
+// // write or storing the image in a  file
 
 // // let file = fs.readFileSync('image.jpg');
 // // console.log(file);
 // // let str = file.toString('base64');
-// // console.log(str);
 
-// write or storing the image in a  file
-
-// let file = fs.readFileSync('image.jpg');
-// console.log(file);
-// let str = file.toString('base64');
-
-// fs.writeFileSync('image1.txt',str);
-str = fs.readFileSync('image.txt',{encoding:'utf-8'});
-let buffer = Buffer.from(str,'base64');
-console.log(buffer);
-fs.writeFileSync('Image1.jpg',buffer);
+// // fs.writeFileSync('image1.txt',str);
+// str = fs.readFileSync('image.txt',{encoding:'utf-8'});
+// let buffer = Buffer.from(str,'base64');
+// console.log(buffer);
+// fs.writeFileSync('Image1.jpg',buffer);
 
 
 
 
-// open a file called "lenna.png"
-// const image = await Jimp.read("image.jpg");
+// // open a file called "lenna.png"
+// // const image = await Jimp.read("image.jpg");
 
-// image.resize(256, 256); // resize
+// // image.resize(256, 256); // resize
 
-// await image.write("image-small.jpg"); // save
-jimp.read('image.jpg', (err, image) => {
-    if (err) throw err;
-    image
-    .resize(257, 256)
+// // await image.write("image-small.jpg"); // save
+
+
+const fs = require('fs');
+const Jimp = require('jimp'); 
+
+
+const base64Str = fs.readFileSync('image.jpg').toString('base64');
+fs.writeFileSync('image.txt', base64Str);
+
+
+const str = fs.readFileSync('image.txt', { encoding: 'utf-8' });
+const buffer = Buffer.from(str, 'base64');
+fs.writeFileSync('Image1.jpg', buffer);
+
+
+Jimp.read('Image1.jpg', function (err, image) {
+  if (err) {
+    console.error('Error reading image:', err);
+    return;
+  }
+
+  image
+    .resize(256, 256)
     .quality(60)
-    .write('imagesmall.jpg');
+    .write('imagesmall.jpg', () => {
+      console.log('Resized image saved as imagesmall.jpg');
+    });
 });
-
