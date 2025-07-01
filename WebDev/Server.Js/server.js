989608fafb818757f5a8add4837980e22665f2a6
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-01 15:50:36 +0530

    Serve static HTML file in server and add index.html
    Added an index.html file with styled content. Updated server.js to serve index.html using fs.readFile instead of sending a hardcoded HTML string.

--- WebDev/Server.Js/server.js
+++ WebDev/Server.Js/server.js
@@ -1,12 +1,20 @@
 const http = require('http');
 const port = 8080;
+const fs = require('fs');
 
 const server =  http.createServer((req,res) =>{
     console.log(req.url)
     res.writeHead(200,{
         "Content-type" : 'text/html'
     });
-    res.end('<h1>hello, here is my requested data</h1>');
+    fs.readFile('./index.html',(err,data)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+        res.end(data);
+    } )
+    // res.end('<h1>hello, here is my requested data</h1>');
 });
 server.listen(port,(err)=>{
     if(err){
