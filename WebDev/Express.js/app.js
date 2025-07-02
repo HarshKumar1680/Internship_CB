afbb6661d56ffa7d91aaf9d85489f5de613708e2
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-02 15:57:00 +0530

    Add /greet endpoint with query parameters, learning req and get
    Introduced a new /greet route that responds with a greeting using 'x' and 'name' query parameters. Also added commented-out code for a potential /greet/:name route.

--- WebDev/Express.js/app.js
+++ WebDev/Express.js/app.js
@@ -2,11 +2,24 @@ const express = require('express');
 const PORT = 8888;
 const app = express();
 
-app.get(`/`,(req,res)=>{
+app.get(`/`, (req, res) => {
     console.log(req);
     res.send(`<h1>Hello Harsh</h1>`);
 })
 
-app.listen(PORT,(err)=>{
+// app.get(`/greet/:name`, (req, res) => {
+//     console.log(req);
+//     res.send(`Hello! ${req.params.name}`);
+
+// })
+
+
+app.get(`/greet`, (req, res) => {
+    console.log(req);
+    res.send(`Hello! Good ${req.query.x} ${req.query.name}`);
+
+})
+
+app.listen(PORT, (err) => {
     console.log(`Server Started at http://localhost:${PORT}`)
 })
