20e7bcbd05852753272471a5ffb2521f1b3be2c4
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-02 18:45:33 +0530

    Add Express form handling example
    Added a simple Express.js app demonstrating form submission using POST and GET methods. Includes an HTML form, server logic to handle /greet routes, and a package.json for project setup.

---
+++ WebDev/Express.js/sendingDatausingForms/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const PORT = 8888;
+
+app.get(`/`, (req, res) => {    
+    res.sendFile(__dirname+ '/index.html')
+
+})
+ app.get('/greet',(req,res)=>{
+    res.send(`Hello! Goodmorning , ${req.query.name}`);
+ })
+
+//  app.post('/greet',(req,res)=>{
+//     res.send(`hello! Goodevening , ${req.query.name}`);
+//  } )
+  app.post('/greet',(req,res)=>{
+    res.send(`hello! Goodevening , ${req.body.name}`);
+ } )
+
+app.listen(PORT, (err) => {
+    console.log(`Server Started at http://localhost:${PORT}`)
+})
