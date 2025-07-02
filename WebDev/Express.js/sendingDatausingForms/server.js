c515b9d3ede6fda29a006ab97b0dfd661a4aaf2f
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-02 20:34:00 +0530

    Add urlencoded middleware to parse form data
    Added express.urlencoded middleware to enable parsing of URL-encoded form data in incoming requests. This is necessary for handling form submissions in Express applications.

--- WebDev/Express.js/sendingDatausingForms/server.js
+++ WebDev/Express.js/sendingDatausingForms/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8888;
 
+app.use(express.urlencoded({extended:true}));
+
 app.get(`/`, (req, res) => {    
     res.sendFile(__dirname+ '/index.html')
 
