d31c3fa6f88dd518a21f67c26db8f0642d72ea53
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-03 16:51:10 +0530

    Add task list functionality with HBS rendering

--- WebDev/TempplateEngineHBS/app.js
+++ WebDev/TempplateEngineHBS/app.js
@@ -5,19 +5,36 @@ const path = require ('path');
 
 // Segtup the hbs engine
 app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended:true}));
 //if folder name is myfolder
 //app.set('views', 'myfolder');
 
 
-app.get(`/`, (req, res) => {    
-    res.sendFile(path.join(__dirname, 'index.html'));
+// app.get(`/`, (req, res) => {    
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// })
+
+// app.get('/learnhbs',(req,res)=>{
+//     res.render('index.hbs', { 
+//         title: "My Page"
+//     });
+// })
+
+let tasklist = [];
+
+app.get('/', (req,res)=>
+{
+    res.render('index',{
+        tasklist
+    })
 })
 
-app.get('/learnhbs',(req,res)=>{
-    res.render('index.hbs', { 
-        title: "My Page"
-    });
+app.post('/addtask', (req,res)=>
+{
+    tasklist.push(req.body.task);
+    res.redirect('/');
 })
+
 
 
 app.listen(PORT, (err) => {
