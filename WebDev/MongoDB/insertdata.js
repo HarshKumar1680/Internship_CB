4dfab7ee2e8ecc4d18d498af124e29aec2a6b6e9
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-05 18:33:03 +0530

    learning crud operations with mongosh

---
+++ WebDev/MongoDB/insertdata.js
@@ -0,0 +1,9 @@
+let users = [
+    {name: 'lovebabbar', subkect: 'web dev',hobby: 'music'},
+    {name: 'codewithharry', subkect: 'java', hobby: 'reading'},
+    {name: 'ritik', subkect: 'c++',hobby: 'badminton'},
+    {name: 'priyanshu', subkect: 'data science',hobby: 'music'},
+    {name: 'priyanshu', subkect: 'deepdata', hobby: 'music'},
+    {name: 'himanshu', subkect: 'deepdata', hobby: 'esports'},
+    {name: 'shivam', subkect: 'deepdata', hobby: 'sports'}
+]
