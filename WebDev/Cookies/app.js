e15190524eaa4392c17dc0ef583fba8ceeb512c1
HarshKumar1680 <harshkumar1680@gmail.com>
2025-07-10 16:23:59 +0530

    cookies in deeep

--- WebDev/Cookies/app.js
+++ WebDev/Cookies/app.js
@@ -1,25 +1,46 @@
 const express = require('express');
 const cookie = require('cookie');
-const path = require('path');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 4444;
 
-
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.setHeader('Set-Cookie',
-        cookie.serialize(
-            'loogedIn', Boolean(true),
-            {
-                httpOnly: true,
-                maxAge: 60 * 60 * 24, // 1 day
-                
-            }));
+    const cookies = cookie.parse(req.headers.cookie || '');
 
-    res.send('Learning Cookies');
+    console.log('From / => loggedIn:', cookies.loggedIn);
+
+    if (cookies.loggedIn === 'true') {
+        return res.redirect('/profile');
+    }
+
+    res.redirect('/login');
 });
 
+app.get('/login', (req, res) => {
+    res.setHeader('Set-Cookie',
+        cookie.serialize('loggedIn', 'true', {
+            maxAge: 15, // 5 seconds for testing
+            path: '/'
+        })
+    );
+
+    res.send("Cookie has been set. <a href='/'>Go Home</a>");
+});
+
+app.get('/profile', (req, res) => {
+    const cookies = cookie.parse(req.headers.cookie || '');
+
+    console.log('From /profile => loggedIn:', cookies.loggedIn);
+
+    if (cookies.loggedIn === 'true') {
+        return res.send('✅ You are inside our app');
+    }
+
+    res.redirect('/login');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
